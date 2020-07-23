bb66c3dbad76ac606583f7ef5ad4561b5f55eddc
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-07-23 03:14:49 -0700

    add query initally for inner join users and tags

--- server.js
+++ server.js
@@ -10,12 +10,28 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query("SELECT 1 + 1 AS solution", (error, results, fields) => {
-   if (error) {
-      console.log(error);
-   } else {
-      console.log("The solution is: ", results[0].solution);
+connection.query(
+   `
+   SELECT 
+        users.id AS user_id, 
+        users.email,
+        xref_user_tags.tag_id 
+    FROM 
+        users 
+    INNER JOIN 
+        xref_user_tags ON user_id = users.id 
+    INNER JOIN 
+        tags ON tags.id = xref_user_tags.tag_id 
+    WHERE 
+        users.id = 'd9d40e8b-d863-4718-87ee-264df43e7057'
+    `,
+   (err, res, fields) => {
+      if (err) {
+         console.log(err);
+      } else {
+         console.log(res);
+      }
    }
-});
+);
 
 connection.end();
