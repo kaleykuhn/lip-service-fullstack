efc0336f25fb57c4e161b089e682e542c2da80a8
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-12-09 16:47:55 -0800

    prepare for deploy

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ app.use(cors());
 
 app.use("/api/v1/users", require("./api/v1/users"));
 app.use("/api/v1/lipsticks", require("./api/v1/lipsticks"));
-app.get("/", (req, res) => res.send("Hello World!"));
+//app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+   res.sendFile(path.resolve(__dirname, "client", "build", "index.html")); // .client/build/index.html
+});
 
 const port = process.env.PORT || 5000;
 
